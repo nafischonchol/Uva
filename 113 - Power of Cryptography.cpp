9ca24ee93a873f636cceb4e8d11458af5bf08fdc
nafischonchol <nafisislam161@gmail.com>
2020-02-18 21:01:23 +0600

    113 Power of Cryptography Solution in C++

---
+++ 113 - Power of Cryptography.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a,b;
+    while(cin>>a)
+    {
+        cin>>b;
+        printf("%.0lf\n",pow(b,1.0/a));
+    }
+    return 0;
+}
